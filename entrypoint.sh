4fef70efda982efff27f57bd53e600acff2e2420
Jules Pietri <heahdude@yahoo.fr>
2023-08-28 18:15:57 +0200

    fix: update tools only when running

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 export PATH=/usr/local/src/vendor/bin:$PATH
 
-composer update --working-dir=/usr/local/src
+export QA_TOOL=`echo "$1" | head -n1 | cut -d " " -f1`
+
+if [ ! -d "${QA_VENDOR_PATH}/$QA_TOOL" ]; then
+  export QA_TOOL="all"
+fi
+
+composer --working-dir=/usr/local/src bin "$QA_TOOL" update
 
 exec $1
